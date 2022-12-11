0d5efb0afb42f3ebb2c457250695adfd76acc336
11ShiSha <83767489+Shishakun@users.noreply.github.com>
2022-12-11 15:12:43 +0300

    +AddHotNews

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-<header class="py-3">
+<header class="py-3 border-b-2 border-black mb-4">
     <div class="w-full flex justify-center">
       <p class="relative font-bold font-rale text-3xl">WARMONGER INC.
         <!-- <img src="../assets/PMC__Wagner_Group__logo.svg" class="ml-16 h-32 w-32"> -->
