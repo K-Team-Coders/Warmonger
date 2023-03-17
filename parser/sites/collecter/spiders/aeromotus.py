87570b6a3b0e32a6a62e23f53544c3cd966e58d8
qTemio <zhitihin.artem1441@yandex.ru>
2023-03-17 23:24:17 +0300

    Update aeromotus.py

--- parser/sites/collecter/spiders/aeromotus.py
+++ parser/sites/collecter/spiders/aeromotus.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 import scrapy
 from loguru import logger
@@ -20,15 +21,17 @@ class AeromotusSpider(scrapy.Spider):
 
     def parse(self, response):
         cards = response.xpath('//div[@class="product-inner product-item__inner"]//a[@class="woocommerce-LoopProduct-link woocommerce-loop-product__link"]/@href')
-        urls = []
+        urls_ = []
         for card in cards:
-            urls.append(card.get())
+            urls_.append(card.get())
         
-        for url in urls:
+        data = urls_[:2]
+        for url in data:
             yield scrapy.Request(url=url+"#tab-specification", method="GET", callback=self.parse_all_urls)
 
     def parse_all_urls(self, response):
-        table = response.xpath('//table')
-        td = table.xpath('//td')
-        for item in td.xpath('//tr'):
-            logger.debug(item.get())
+        th = response.xpath('//table//tr//th/text()').getall()
+        td = response.xpath('//table//tr//td/text()').getall()
+
+        for index in range(len(th)):
+            logger.success(th[index] + " || " + td[index])
