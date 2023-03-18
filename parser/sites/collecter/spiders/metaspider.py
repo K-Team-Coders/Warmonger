04c69f03b803ab237475ecb2c22e649e419d57ba
qTemio <zhitihin.artem1441@yandex.ru>
2023-03-18 18:46:24 +0300

    Update metaspider.py

--- parser/sites/collecter/spiders/metaspider.py
+++ parser/sites/collecter/spiders/metaspider.py
@@ -28,29 +28,53 @@ class MetaSpider(scrapy.Spider):
             password=PASSWORD,
             dbname=DBNAME
         )
+        self.rubbish = ["policy", "instagram", "terms", "footer", "download", "service", "youtube", "login", "pdf", "img", "png", "sign", "support", "logout","faq", "blog", "develop","javascript", "accounts", "auth", "user", "forum", "contacts", "facebook", "#page", "cart", "tel", "#", "@", "company", "personal", "action"]
         self.cursor = self.connection.cursor()
         self.visited = []
+        self.domens = []
 
         urls = [
             'https://aeromotus.ru/', 
-            "https://nelk.ru", 
+            "https://nelk.ru/", 
             "https://dji.com/ru/dji-fpv/specs", 
-            "https://geobox.ru"
+            "https://geobox.ru/"
         ]
 
         for url in urls:
+            left = url.find('//') + 2
+            right = url[left:].find('/') + left
+            self.domens.append(url[left:right])
             yield scrapy.Request(url=url, method="GET", callback=self.parse)
 
     def parse(self, response):
+        logger.debug(response.url)
         self.visited.append(response.url)
         urls = response.css('a::attr(href)').extract()
-        logger.debug(urls)
         
+        cleaned = []
         for url in urls:
-            if url not in self.visited:
-                yield scrapy.Request(url=url, method="GET", callback=self.parse_with_extraction)
+            checker = False
+            for item in self.rubbish:
+                if item in url:
+                    checker = True
+
+            domen_checker = False
+            for domen in self.domens:
+                if domen in url:
+                    domen_checker = True
+            if not checker and domen_checker:
+                cleaned.append(url)
+
+        logger.debug(len(urls))
+        logger.debug(len(cleaned))
+
+        logger.debug(cleaned)
+        # for url in urls:
+            # if url not in self.visited:
+                # yield scrapy.Request(url=url, method="GET", callback=self.parse_with_extraction)
 
     def parse_with_extraction(self, response):
+        logger.debug(response.url)
         self.visited.append(response.url)
         urls = response.css('a::attr(href)').extract()
         # logger.debug(urls)
@@ -59,7 +83,7 @@ class MetaSpider(scrapy.Spider):
         logger.success(type(uls))
         table = response.xpath('//table').getall()
         logger.success(table)
-        
+
         for url in urls:
             if url not in self.visited:
                 yield scrapy.Request(url=url, method="GET", callback=self.parse_with_extraction)
